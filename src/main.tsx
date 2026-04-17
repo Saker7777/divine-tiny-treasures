a5b1eb6ef5d6dacdf6d72303df06ab40191b00aa
Saker7777 <125402809+Saker7777@users.noreply.github.com>
2026-04-17 19:36:43 -0300

    Set up React application with RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { RouterProvider } from "@tanstack/react-router"
+import { router } from "./router"
+import "./styles.css"
 
+const root = ReactDOM.createRoot(document.getElementById("root")!)
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
